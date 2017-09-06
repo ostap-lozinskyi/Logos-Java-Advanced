840a0f226ccd54e3603f35410552085e219ff92a
Ostap <Ostap.lviv@gmail.com>
2017-09-06 18:41:13 +0300

    Signed-off-by: Ostap <Ostap.lviv@gmail.com>

--- Adv14_using_Pagination/src/main/java/ua/repository/IngredientRepository.java
+++ Adv14_using_Pagination/src/main/java/ua/repository/IngredientRepository.java
@@ -2,6 +2,8 @@ package ua.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 
 import ua.entity.Ingredient;
@@ -11,6 +13,10 @@ public interface IngredientRepository extends JpaNameRepository<Ingredient> {
 
 	@Query("SELECT new ua.model.view.IngredientView(i.id, i.name) FROM Ingredient i")
 	List<IngredientView> findAllView();
+	
+	@Query(value = "SELECT new ua.model.view.IngredientView(i.id, i.name) FROM Ingredient i", 
+			countQuery = "SELECT count(i.id) FROM Ingredient i")
+	Page<IngredientView> findAllView(Pageable pageable);
 	
 	@Query("SELECT i FROM Ingredient i WHERE i.id=?1")
 	Ingredient findOneRequest(Integer id);
