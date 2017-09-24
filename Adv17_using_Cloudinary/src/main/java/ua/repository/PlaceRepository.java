9e747fa7fd54d85285cce357e5084912e3af73bd
Ostap lozinskyj <ostap.lviv@gmail.com>
2017-09-24 17:31:22 +0300

    adv17

--- Adv17_using_Cloudinary/src/main/java/ua/repository/PlaceRepository.java
+++ Adv17_using_Cloudinary/src/main/java/ua/repository/PlaceRepository.java
@@ -13,7 +13,7 @@ public interface PlaceRepository extends JpaRepository<Place, Integer> {
 	@Query("SELECT new ua.model.view.PlaceView(p.id, p.countOfPeople, p.number) FROM Place p")
 	List<PlaceView> findAllPlaceViews();
 	
-	@Query("SELECT p.countOfPeople FROM Place p")
+	@Query("SELECT distinct p.countOfPeople FROM Place p")
 	List<String> findAllPlacesCountOfPeople();
 	
 	@Query("SELECT p FROM Place p WHERE p.id=?1")
